894a5ef3375fc5cf6925ddc202836a5554ee4650
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 20:14:25 -0300

    TreinoReact

---
+++ TreinoReact/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function Gama() {
+    
+  return (
+    <>
+      <header>
+        <h1>GAMA</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+      
+    </main>
+    </>
+  )
+}
+
+export default Gama
